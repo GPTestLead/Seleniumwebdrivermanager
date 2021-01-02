5a0752a6bf55ba57826cd672d15b2062ab278cda
Garvita <garvita160109@gmail.com>
2021-01-01 21:42:04 -0600

    Seleniumwebdrivermanager First Commit

---
+++ src/main/java/Seleniumwebdrivermanager/Seleniumwebdrivermanager/App.java
@@ -0,0 +1,13 @@
+package Seleniumwebdrivermanager.Seleniumwebdrivermanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
